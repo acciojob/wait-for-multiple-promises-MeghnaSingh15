d1844d57e9e22bcb20068c8e7e2588124b18fd8b
Meghna Singh <138083187+MeghnaSingh15@users.noreply.github.com>
2024-10-18 00:15:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const Promise1 = new Promise1((resolve,reject) =>
+	setTimeout(() => {
+		console.log("Promise1 resolved");
+	},1000)
+);
+const Promise2 = new Promise2((resolve,reject) =>
+	setTimeout(() => {
+		console.log("Promise1 resolved");
+	},2000)
+);
+const Promise3 = new Promise3((resolve,reject) =>
+	setTimeout(() => {
+		console.log("Promise1 resolved");
+	},1500)
+);
+Promise.all([Promise1, Promise2, Promise3])
+.then(() => {
+	console.log("All promises resolved");
+});
